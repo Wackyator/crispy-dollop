7bbf8f3c980df7b611de98010b1a5a5adad3e132
Wackyator <pratapsinghtarun9@gmail.com>
2024-10-18 19:43:12 +0530

    fix: use BookPub model in get_books route

--- app/api/v1/books.py
+++ app/api/v1/books.py
@@ -9,13 +9,13 @@ from app.db import DB
 router = APIRouter(prefix="/books", tags=["books"])
 
 
-@router.get("/")
+@router.get("/", response_model=list[models.BookPub])
 async def get_books(
     db: DB,
     offset: int = 0,
     limit: Annotated[int, Query(le=100)] = 100,
-) -> list[models.Book]:
-    books = list(db.exec(select(models.Book).offset(offset).limit(limit)).all())
+) -> Any:
+    books = db.exec(select(models.Book).offset(offset).limit(limit)).all()
     return books
 
 
@@ -47,7 +47,7 @@ async def update_book(db: DB, book_id: int, book: models.BookUpdate) -> Any:
     if not db_book:
         raise HTTPException(status_code=404, detail="Book not found")
 
-    # could be done better 
+    # could be done better
     book_data = book.model_dump(exclude_unset=True)
     db_book.sqlmodel_update(book_data)
 
